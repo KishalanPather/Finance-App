edda7e7f6664c2fe78d90ee9917933c0b73245e0
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2026-07-20 21:18:42 +0200

    added unit tests for the increasing account balance and mapping the correct response dto

--- src/test/java/com/kish/financeapp/Transactions/TransactionServiceTest.java
+++ src/test/java/com/kish/financeapp/Transactions/TransactionServiceTest.java
@@ -1,5 +1,6 @@
 package com.kish.financeapp.Transactions;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -19,6 +20,7 @@ import com.kish.financeapp.Accounts.Account;
 import com.kish.financeapp.Accounts.AccountRepository;
 import com.kish.financeapp.Accounts.enums.AccountType;
 import com.kish.financeapp.Transactions.dtos.AddIncomeRequestDto;
+import com.kish.financeapp.Transactions.dtos.TransactionResponseDto;
 import com.kish.financeapp.Transactions.enums.TransactionType;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +38,7 @@ public class TransactionServiceTest {
 
     @Test
     public void shouldSaveIncomeTransaction(){
+        //Arrange: Build dto
         AddIncomeRequestDto incomeRequest = new AddIncomeRequestDto(
             1,
             BigDecimal.valueOf(100),
@@ -45,8 +48,89 @@ public class TransactionServiceTest {
             "Monthly salary"
         );
 
-        Transaction saved = Transaction.builder()
-                .envelopeId(null)
+        //build what the database returns, if not here, code will throw an error as unable to save a transaction.
+        Transaction saved = new Transaction();  //empty bc content not important for this test
+
+        //build an account so getAccountById returns something
+        Account account = new Account();
+        account.setAvailableBalance(BigDecimal.valueOf(400));   //only concerned with the balance field 
+        
+        //mock
+        when(accountRepository.findById(1))
+            .thenReturn(Optional.of(account));
+        
+
+        when(transactionRepository.save(any(Transaction.class)))
+            .thenReturn(saved);
+
+        
+        //act
+        transactionService.addIncome(incomeRequest);
+
+        //assert: ensure the transaction was saved
+        verify(transactionRepository).save(any(Transaction.class));
+
+        
+    }
+
+    @Test
+    public void shouldIncreaseAccountBalance(){
+        // will need to arrange an income dto, and account with a balance.
+        // mock the findbyId account repository method and transaction.
+        // assert account availableBalance 
+
+
+        //arrange
+        AddIncomeRequestDto incomeRequest = new AddIncomeRequestDto(
+            1,
+            BigDecimal.valueOf(100),
+            TransactionType.INCOME,
+            "Salary",
+            "Job",
+            "Monthly salary"
+        );
+        
+        Account account = new Account();
+        account.setAvailableBalance(BigDecimal.valueOf(400));   //only concerned about the balance in this test.
+
+        Transaction transaction = new Transaction();            //empty, bc content not important for this test.
+
+        //mock
+        when(accountRepository.findById(1))
+            .thenReturn(Optional.of(account));
+
+         when(transactionRepository.save(any(Transaction.class)))
+        .thenReturn(transaction);
+
+        //act
+        transactionService.addIncome(incomeRequest);
+
+        //assert
+        assertEquals(BigDecimal.valueOf(500), account.getAvailableBalance());
+
+
+
+    }
+
+    @Test
+    public void shouldReturnTransactionResponseDto(){
+
+        //arrange
+        AddIncomeRequestDto incomeRequest = new AddIncomeRequestDto(
+            1,
+            BigDecimal.valueOf(100),
+            TransactionType.INCOME,
+            "Salary",
+            "Job",
+            "Monthly salary"
+        );
+
+        Account account = new Account();
+        account.setAvailableBalance(BigDecimal.valueOf(400)); //only balance needed for this test
+
+        Transaction transaction = Transaction.builder()
+            .transactionId(5)
+            .envelopeId(null)
                 .accountId(incomeRequest.accountId())
                 .amount(incomeRequest.amount())
                 .description(incomeRequest.description())
@@ -57,32 +141,27 @@ public class TransactionServiceTest {
                 .build();
 
 
-        Account account = new Account(
-            1,
-            "Sample Account",
-            AccountType.DEBIT,
-            BigDecimal.valueOf(400)
-        );
-        
+
+        //mock
         when(accountRepository.findById(1))
             .thenReturn(Optional.of(account));
-        
 
         when(transactionRepository.save(any(Transaction.class)))
-            .thenReturn(saved);
+        .thenReturn(transaction);
 
-        transactionService.addIncome(incomeRequest);
+        //act
+         TransactionResponseDto response = transactionService.addIncome(incomeRequest);
 
-        verify(transactionRepository).save(any(Transaction.class));
+         //assert
+         assertEquals(transaction.getTransactionId(), response.transactionId());
+         assertEquals(transaction.getEnvelopeId(), response.envelopeId());
+         assertEquals(transaction.getAccountId(), response.accountId());
+         assertEquals(transaction.getAmount(), response.amount());
+         assertEquals(transaction.getDescription(),response.description());
+         assertEquals(transaction.getCategory(),response.category());
+         assertEquals(transaction.getNote(),response.note());
 
-        
     }
-
-    @Test
-    public void shouldIncreaseAccountBalance(){}
-
-    @Test
-    public void shouldReturnTransactionResponseDto(){}
 
     @Test
     public void shouldThrowExceptionWhenAmountIsNegative(){}
