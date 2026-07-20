9410f5c263230e65a4779d10b357d38cc41609d5
KishalanPather <119253561+KishalanPather@users.noreply.github.com>
2026-07-20 20:45:54 +0200

    finished shouldSaveIncomeTransaction unit test

--- src/test/java/com/kish/financeapp/Transactions/TransactionServiceTest.java
+++ src/test/java/com/kish/financeapp/Transactions/TransactionServiceTest.java
@@ -4,11 +4,22 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.lang.foreign.Linker.Option;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.kish.financeapp.Accounts.Account;
+import com.kish.financeapp.Accounts.AccountRepository;
+import com.kish.financeapp.Accounts.enums.AccountType;
+import com.kish.financeapp.Transactions.dtos.AddIncomeRequestDto;
+import com.kish.financeapp.Transactions.enums.TransactionType;
 
 @ExtendWith(MockitoExtension.class)
 public class TransactionServiceTest {
@@ -16,21 +27,51 @@ public class TransactionServiceTest {
     @Mock
     private TransactionRepository transactionRepository;
 
+    @Mock
+    private AccountRepository accountRepository;
+
     @InjectMocks
     private TransactionService transactionService;
 
 
     @Test
     public void shouldSaveIncomeTransaction(){
-        AddIncomeRequestDto request = new AddIncomeRequestDto();
+        AddIncomeRequestDto incomeRequest = new AddIncomeRequestDto(
+            1,
+            BigDecimal.valueOf(100),
+            TransactionType.INCOME,
+            "Salary",
+            "Job",
+            "Monthly salary"
+        );
 
-        Transaction saved = new Transaction();
+        Transaction saved = Transaction.builder()
+                .envelopeId(null)
+                .accountId(incomeRequest.accountId())
+                .amount(incomeRequest.amount())
+                .description(incomeRequest.description())
+                .transactionType(incomeRequest.transactionType())
+                .category(incomeRequest.category())
+                .date(new Date())
+                .note(incomeRequest.note())
+                .build();
 
+
+        Account account = new Account(
+            1,
+            "Sample Account",
+            AccountType.DEBIT,
+            BigDecimal.valueOf(400)
+        );
+        
+        when(accountRepository.findById(1))
+            .thenReturn(Optional.of(account));
+        
 
         when(transactionRepository.save(any(Transaction.class)))
             .thenReturn(saved);
 
-        transactionService.addIncome(request);
+        transactionService.addIncome(incomeRequest);
 
         verify(transactionRepository).save(any(Transaction.class));
 
